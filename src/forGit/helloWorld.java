dcfede83c1e7910c7209ee65a030bd8ad0baa6f8
nikomiso <nikomiso92@gmail.com>
2014-11-05 22:22:59 +0200

    hello world commit.

---
+++ src/forGit/helloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+/**
+ * @author Nikos
+ *
+ */
+package forGit;
+
+public class helloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("hello world");
+	
+	}
+}
